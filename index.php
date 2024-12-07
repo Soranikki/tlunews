92a825696584b37e689e22d296debf1717d78165
Ha2308 <145110590+Ha2308@users.noreply.github.com>
2024-12-07 15:17:44 +0700

    Revert "Commit này ông thêm sửa xoá cái gì ghi ở đây"

--- index.php
+++ index.php
@@ -5,12 +5,8 @@
     <meta name="viewport" content="width=
     , initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <div class="container">
-
-    <h1>Yahoo</h1>
-    </div>
+    <h1>hdjsdoaod</h1>
 </body>
 </html>
